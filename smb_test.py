bc5f3a65eb3c5aac412f9cfd94489e5dd7156054
Daniil Sapunov <63109470+kakadu228@users.noreply.github.com>
2024-09-27 01:27:10 +0300

    Update smb_test.py

--- smb_test.py
+++ smb_test.py
@@ -2,15 +2,15 @@ from smb.SMBConnection import SMBConnection
 from smb.smb_structs import OperationFailure
 
 def main():
-    username = "test"  # Замените на ваше имя пользователя
-    password = "P@ssw0rd"    # Замените на ваш пароль
+    username = "Username"  # Замените на ваше имя пользователя
+    password = "Pass"    # Замените на ваш пароль
     local_name = "KaliMachine"     # Имя вашей машины
-    remote_name = "WS19"  # Имя удалённой машины
+    remote_name = "Hostname_server"  # Имя удалённой машины
 
-    conn = SMBConnection(username, password, local_name, remote_name, domain="WORKGROUP", use_ntlm_v2=True)
+    conn = SMBConnection(username, password, local_name, remote_name, domain="none", use_ntlm_v2=True)
 
     try:
-        conn.connect("192.168.1.14", 445)  # Замените на IP адрес вашего Windows Server
+        conn.connect("IP", 445)  # Замените на IP адрес вашего Windows Server
         print("Подключение успешно!")
         # Ваш код для обработки запросов будет здесь
     except OperationFailure as e:
